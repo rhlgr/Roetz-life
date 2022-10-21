5c4c17a2f21a387bb650754480dca1ee970f0415
rhlgr1 <rg512g1@gmail.com>
2022-10-21 15:22:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const larrow = document.querySelector(".b3img1");
+const rarrow = document.querySelector(".b3img2");
+const b3one = document.querySelector(".b3one");
+const b3two = document.querySelector(".b3two");
+const b3three = document.querySelector(".b3three");
+const b3four = document.querySelector(".b3four");
+const b3middle = document.querySelector(".b3middle");
+
+rarrow.addEventListener("click")
+
+
+
+
+// $('.b3middle').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1
+//   });
+  
+
+// $(document).ready(function(){
+//     $('.b3middle').slick({
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       autoplay: true,
+//       autoplaySpeed: 2000,
+//       dots:true,
+//     });
+//   });
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
